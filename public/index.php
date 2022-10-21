c22b23fde663d61448bfc6d526be7ecdbda409d3
AntaresMugisho <antaresmugisho@gmail.com>
2022-10-21 10:20:33 +0200

    Setting up scss to design views

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
     require "../vendor/autoload.php";
+    require "../private/index.php";
 
-    $url = "http://api.openweathermap.org/data/2.5/forecast?q=Bujumbura&appid={$API_KEY}";
+    $city = "Bujumbura";
+    $lang = "fr";
+    $url = "http://api.openweathermap.org/data/2.5/forecast?q={$city}&lang={$lang}&appid=". API_KEY;
+
+    var_dump($url);
+    exit();
+
     $json = file_get_contents($url);
-
     $data = json_decode($json);
 ?>
 
