a0b62a014110c317f30c3e4585ae46e52b3fe9eb
AntaresMugisho <antaresmugisho@gmail.com>
2022-10-23 19:51:32 +0200

    Navbar and footer design done

--- public/index.php
+++ public/index.php
@@ -4,13 +4,12 @@
 
     $city = "Bujumbura";
     $lang = "fr";
-    $url = "http://api.openweathermap.org/data/2.5/forecast?q={$city}&lang={$lang}&appid=". API_KEY;
+    // $url = "http://api.openweathermap.org/data/2.5/forecast?q={$city}&lang={$lang}&appid=". API_KEY;
 
-    var_dump($url);
-    exit();
-
-    $json = file_get_contents($url);
-    $data = json_decode($json);
+    // $json = file_get_contents($url);
+    // $data = json_decode($json);
+    $data = file_get_contents("data.txt");
+    // $data = json_decode($data);
 ?>
 
 <pre>
