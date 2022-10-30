3f787474f1645a34b0acbbbb2f9a4a09638f8b01
AntaresMugisho <antaresmugisho@gmail.com>
2022-10-30 13:35:56 +0200

    Daily forecast interfaces done

--- public/index.php
+++ public/index.php
@@ -1,21 +1,23 @@
 <?php
+
+
+
     require "../vendor/autoload.php";
     require "../private/index.php";
 
     $city = "Bujumbura";
     $lang = "fr";
-    // $url = "http://api.openweathermap.org/data/2.5/forecast?q={$city}&lang={$lang}&appid=". API_KEY;
+    $url = "http://api.openweathermap.org/data/2.5/forecast?q={$city}&lang={$lang}&appid=". API_KEY;
 
     // $json = file_get_contents($url);
-    // $data = json_decode($json);
-    $data = file_get_contents("data.txt");
-    // $data = json_decode($data);
+    $json = file_get_contents("../private/data.json");
+    $data = json_decode($json);
 
-    
+   
 ?>
 
 <pre>
-    <?php var_dump($data); ?> 
+    <?php  var_dump($json);  ?> 
 </pre>
     
 
